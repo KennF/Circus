0edfbc8df4cd984254b012203383f6090b54b326
kennf <cactus.fxy@gmail.com>
2013-11-01 00:30:41 +0800

    add development media server

--- develop/fun/tango/rango/urls.py
+++ develop/fun/tango/rango/urls.py
@@ -5,3 +5,5 @@ urlpatterns = patterns("",
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     )
+
+
--- develop/fun/tango/rango/views.py
+++ develop/fun/tango/rango/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
@@ -9,4 +8,7 @@ def index(request):
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-    return HttpResponse('Rango says: Here is the about page! Return to <a href="/rango/">rango home page</a>')
+    # return HttpResponse('Rango says: Here is the about page! Return to <a href="/rango/">rango home page</a>')
+    context = RequestContext(request)
+    context_dict = {}
+    return render_to_response('rango/about.html', context_dict, context)
