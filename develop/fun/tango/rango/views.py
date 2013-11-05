cc146566d53c277ede49ebb1d0d75bd96ad3b9ef
kennf <cactus.fxy@gmail.com>
2013-11-06 00:18:33 +0800

    add most viewed pages in index.html

--- develop/fun/tango/rango/views.py
+++ develop/fun/tango/rango/views.py
@@ -11,16 +11,17 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     for category in category_list:
-        category.url = category.name.replace(" ", "_")
-    context_dict = {'categories': category_list}
+        category.url = encoded_url(category.name)
+    context_dict = {'categories': category_list, 'pages': page_list}
     # Render the response and send it back!
     return render_to_response('rango/index.html', context_dict, context)
 
 def category(request, category_name_url):
     context = RequestContext(request)
 
-    category_name = category_name_url.replace('_', '')
+    category_name = decoded_url(category_name_url)
     context_dict = {'category_name': category_name}
     try:
         category = Category.objects.get(name=category_name)
@@ -33,9 +34,15 @@ def category(request, category_name_url):
         pass
     return render_to_response('rango/category.html', context_dict, context)
 
-
 def about(request):
     # return HttpResponse('Rango says: Here is the about page! Return to <a href="/rango/">rango home page</a>')
     context = RequestContext(request)
     context_dict = {}
     return render_to_response('rango/about.html', context_dict, context)
+
+def encoded_url(url):
+    return url.replace(" ", "_")
+
+def decoded_url(url):
+    return url.replace("_", " ")
+
