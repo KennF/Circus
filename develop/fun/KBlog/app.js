8cdc7d152b19adb52214bd5db3b79a0d481e984e
ken <ken@autodesk.com>
2014-02-19 18:14:34 +0800

    add basic route

--- develop/fun/KBlog/app.js
+++ develop/fun/KBlog/app.js
@@ -18,7 +18,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
+app.use(express.router(routes));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development
--- develop/fun/KBlog/routes/index.js
+++ develop/fun/KBlog/routes/index.js
@@ -4,6 +4,21 @@
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.render('index', { title: "Hello", test: ["hello", "world"]});
+        res.render('index', { title: "Home Page"} );
+    });
+
+    app.get('/reg', function (req, res) {
+        res.render('reg', { title: "Registry" });
+    });
+
+    app.get('/login', function (req, res) {
+        res.render('login', { title: "Login" });
+    });
+
+    app.get('/post', function (req, res) {
+        res.render('post', { title: "Post" });
+    });
+
+    app.get('/login', function (req, res) {
     });
 };
