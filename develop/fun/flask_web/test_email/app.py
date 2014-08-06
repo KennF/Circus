17a7038f43579a90393c5a185a3c7c7c4cba58d1
kennf <cactus.fxy@gmail.com>
2014-08-07 00:06:30 +0800

    update test_mail in flask learn - move Mail init after app.config setting

--- develop/fun/flask_web/test_email/app.py
+++ develop/fun/flask_web/test_email/app.py
@@ -5,19 +5,22 @@ from flask.ext.script import Manager
 from flask.ext.mail import Mail, Message
 
 app = Flask(__name__)
-mail = Mail(app)
+
 manager = Manager(app)
 
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
 app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <cactus.fxy@gmail.com>'
 
+#!!!! It's VERY IMPORTANT - to init mail after setting app.config !!!
+mail = Mail(app)
 
 def send_async_email(app, msg):
     with app.app_context():
